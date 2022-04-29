b4d7644ea2341e6776f0c15c06002ffd08de112d
Shashank Agrawal <56279418+ShashankAgrawal003@users.noreply.github.com>
2022-04-29 14:48:02 +0530

    Update nav_bar_fac.dart

--- lib/nav_bar_fac.dart
+++ lib/nav_bar_fac.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_attendence_system/account_setting.dart';
 import 'package:qr_code_attendence_system/apply_leave.dart';
-
 import 'package:qr_code_attendence_system/notes_upload.dart';
 import 'package:qr_code_attendence_system/modify_attendance.dart';
 
 class NavBarFac extends StatelessWidget {
   const NavBarFac({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,38 +14,23 @@ class NavBarFac extends StatelessWidget {
         children: [
           UserAccountsDrawerHeader(
             accountName: Text(
-              'Asheesh Tiwari',
-            ),
+              'Asheesh Tiwari', ),
             accountEmail: Text(
-              'asheesh@gla.ac.in',
-            ),
+              'asheesh@gla.ac.in', ),
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
                 child: Image.asset('images/asheesh_tiwari_sir.jpg',
-                    width: 90, height: 90, fit: BoxFit.cover),
-              ),
+                    width: 90, height: 90, fit: BoxFit.cover), ),
             ),
             decoration: BoxDecoration(
               color: Colors.blue,
               image: DecorationImage(
                 image: AssetImage(
-                  'images/app-background.jpg',
-                ),
+                  'images/app-background.jpg', ),
                 fit: BoxFit.cover,
               ),
             ),
           ),
-
-          // ListTile(
-          //   leading: Icon(Icons.notifications),
-          //   title: Text('Check Attendence'),
-          //   onTap: () => null,
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.notifications),
-          //   title: Text('Mark Youself Present'),
-          //   onTap: () => null,
-          // ),
           Divider(),
           ListTile(
             leading: Icon(Icons.account_box),
@@ -67,11 +50,6 @@ class NavBarFac extends StatelessWidget {
             onTap: () => Navigator.of(context)
                 .push(MaterialPageRoute(builder: (context) => ApplyLeave())),
           ),
-          // ListTile(
-          //   leading: Icon(Icons.notifications),
-          //   title: Text('Prediction on Absent'),
-          //   onTap: () => null,
-          // ),
           Divider(),
           ListTile(
             leading: Icon(Icons.settings),
